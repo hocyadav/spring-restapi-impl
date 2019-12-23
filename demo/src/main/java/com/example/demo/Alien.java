f5f6843091aa7f2d2a999a3dcebe341ade360319
Hariom Yadav <hariom.yadav@broadcom.com>
2019-12-23 09:05:21 +0530

    Spring project : restful api , get + post + postgre db

---
+++ demo/src/main/java/com/example/demo/Alien.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Alien {
+	
+	@Id
+	int id;
+	String name;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "Alien [id=" + id + ", name=" + name + "]";
+	}
+	
+	
+	
+	
+}
---
+++ demo/src/main/java/com/example/demo/Resource.java
@@ -0,0 +1,72 @@
+package com.example.demo;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Resource {
+	
+	@Autowired
+	DbPostgres repo;
+	
+//	@GetMapping
+//	public List<Alien> fun() {
+//		
+//		List<Alien> list = new LinkedList<>();
+//		
+//		
+//		Alien obj = new Alien();
+//		obj.id = 1;
+//		obj.name = "hari";
+//		
+//		Alien obj2 = new Alien();
+//		obj2.id = 2;
+//		obj2.name = "yadav";
+//		
+//		list.add(obj);
+//		list.add(obj2);
+//		
+//		
+//		return list;
+//	}
+	
+	@PostMapping
+	public List<Alien> fun2() {
+		
+		List<Alien> list = new LinkedList<>();
+		
+		
+		Alien obj = new Alien();
+		obj.id = 3;
+		obj.name = "hari";
+		
+		Alien obj2 = new Alien();
+		obj2.id = 4;
+		obj2.name = "yadav";
+		
+		list.add(obj);
+		list.add(obj2);
+		
+		
+		return list;
+	}
+	
+	
+	@GetMapping("db")
+	public List<Alien> fun3() {
+		
+		//List<Alien> list = new LinkedList<>();
+		
+		
+		List<Alien> list  = (List<Alien>) repo.findAll();
+		
+		
+		return list;
+	}
+	
+}
